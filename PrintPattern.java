af02a403b967ec73808fa7fb4784b41b1a065a94
parthbathia <parthbathia@gmail.com>
2016-05-25 23:23:12 +0530

    Add files via upload

---
+++ PrintPattern.java
@@ -0,0 +1,27 @@
+class PrintPattern{
+
+	public static void main(String[] args){
+
+		for(int i=1;i<6;i++){
+
+			for(int j=1;j<=i;j++){
+
+				System.out.print(j);
+			
+			}
+			System.out.println();
+		}
+
+		for(int i=5;i>0;i--){
+
+			for(int j=1;j<i;j++){
+
+				System.out.print(j);			
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
